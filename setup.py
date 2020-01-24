90804a681d36cfbf6a6361890c976d7c6655db6e
commputethis <david@prowshouse.com>
2020-01-23 19:33:01 -0500

    update plugin name

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "usbcontrol"
+plugin_identifier = "usbcontrolv2"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "usbcontrol"
+plugin_package = "octoprint_usbcontrolv2"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "USBControl"
+plugin_name = "USBControlv2"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "1.0.7"
